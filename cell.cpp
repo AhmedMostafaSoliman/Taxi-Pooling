6b2d91a0c66e11db8c2e42215bf32b2bcdc3ffaa
Amr Mohamed <amr_mohamed@live.com>
2016-05-08 00:16:14 +0200

    Move the implementation of the functions to the cell.cpp file

--- cell.cpp
+++ cell.cpp
@@ -1,1 +1,38 @@
 #include "cell.h"
+
+cell::cell (QWidget *parent) : QLabel (parent),
+    currentState (Road)
+{
+    ImageUpd();
+}
+cell::State cell::getState() { return currentState; }
+bool cell::isRoad() { return currentState==cell::Road; }
+
+void cell::setState(State newState)
+{
+    if (currentState != newState)
+    {
+        currentState = newState;
+        ImageUpd();
+    }
+}
+
+void cell::ImageUpd()
+{
+    QPixmap roadPixmap(":/images/road.jpg");
+    QPixmap pavPixmap(":/images/pav.jpg");
+    QPixmap taxiPixmap(":/images/taxi.png");
+    QPixmap customerPixmap(":/images/customer.png");
+
+
+    int w=128,h=128;
+
+    switch (currentState)
+    {
+        case Pavement: setPixmap(pavPixmap.scaled(w,h,Qt::KeepAspectRatio)); break;
+        case Road: setPixmap(roadPixmap.scaled(w,h,Qt::KeepAspectRatio)); break;
+        case Taxi:  setPixmap(taxiPixmap.scaled(w,h,Qt::KeepAspectRatio)); break;
+        case Customer:  setPixmap(customerPixmap.scaled(w,h,Qt::KeepAspectRatio)); break;
+
+    }
+}
--- cell.h
+++ cell.h
@@ -5,50 +5,16 @@
 class cell : public QLabel
 {
     Q_OBJECT
-
 public:
     enum State {Pavement, Road , Taxi ,Customer};
-
-    cell (QWidget *parent = 0) : QLabel (parent),
-        currentState (Road)
-    {
-        ImageUpd();
-    }
-
-    State getState() { return currentState; }
-    bool isRoad() { return currentState==cell::Road; }
-
-    void setState(State newState)
-    {
-        if (currentState != newState)
-        {
-            currentState = newState;
-            ImageUpd();
-        }
-    }
+    cell (QWidget *parent = 0) ;
+    State getState();
+    bool isRoad();
+    void setState(State newState);
 
 private:
-    void ImageUpd()
-    {
-        QPixmap roadPixmap(":/images/road.jpg");
-        QPixmap pavPixmap(":/images/pav.jpg");
-        QPixmap taxiPixmap(":/images/taxi.png");
-        QPixmap customerPixmap(":/images/customer.png");
-
-
-        int w=128,h=128;
-
-        switch (currentState)
-        {
-            case Pavement: setPixmap(pavPixmap.scaled(w,h,Qt::KeepAspectRatio)); break;
-            case Road: setPixmap(roadPixmap.scaled(w,h,Qt::KeepAspectRatio)); break;
-            case Taxi:  setPixmap(taxiPixmap.scaled(w,h,Qt::KeepAspectRatio)); break;
-            case Customer:  setPixmap(customerPixmap.scaled(w,h,Qt::KeepAspectRatio)); break;
-
-        }
-    }
-
+    void ImageUpd();
     State currentState;
 };
 
-#endif // TAXICAR_H
+#endif // CELL_H
